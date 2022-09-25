8fc6955d0959d7a76dbcd40ab114bb800dec503a
Snehal Balpande <snehalbalpande1@gmail.com>
2022-09-25 21:54:38 +0530

    added setup

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
-from setuptools import setup,find_packages
+from setuptools import setup
 from typing import List
 
 #Declaring variables for setup functions
-PROJECT_NAME="income_prediction"
-VERSION="0.0.2"
-AUTHOR="snehal balpande"
-DESRCIPTION="This is a adult income_prediction Machine Learning Project"
-
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Avnish Yadav"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
-HYPHEN_E_DOT = "-e ."
 
-
-def get_requirements_list() -> List[str]:
+def get_requirements_list()->List[str]:
     """
-    Description: This function is going to return list of requirement
+    Description: This function is going to return list of requirement 
     mention in requirements.txt file
-    return This function is going to return a list which contain name
+    return This function is going to return a list which contain name 
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
-        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-        if HYPHEN_E_DOT in requirement_list:
-            requirement_list.remove(HYPHEN_E_DOT)
-        return requirement_list
-
+        return requirement_file.readlines()
 
 
 setup(
@@ -33,6 +26,7 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=find_packages(), 
+packages=PACKAGES,
 install_requires=get_requirements_list()
+
 )
